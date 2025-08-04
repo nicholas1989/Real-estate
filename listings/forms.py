46da68bbdb7e19d88d12991597a4032b7727af8d
nicholas1989 <nwokoronicholas@yahoo.com>
2025-08-04 01:32:57 +0000

    listing-create-view complete

---
+++ listings/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from .models import Listing
+
+
+class ListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = [
+            "title",
+            "address",
+            "price",
+            "square_footage",
+            "num_rooms"
+        ]
--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Listing
+from .forms import ListingForm
 
 # Create your views here.
 def listing_list(request):
@@ -10,11 +11,24 @@ def listing_list(request):
     return render(request, "listings.html", context)
 
 
-
 def listing_retrieve(request, pk):
     listing = Listing.objects.get(id=pk)
     context = {
         "listing": listing
     }
     return render(request, "listing.html", context)
+
+
+def listing_create(request):
+    form = ListingForm()
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        #print(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    context = {
+        "form": form
+    }
+    return render(request, "listing_create.html", context)
     
